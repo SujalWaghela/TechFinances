40f4508988410c2c5c89540125ca421a7ce9285a
Deep Panchal <deep@Deeps-MacBook-Air.local>
2026-09-20 15:33:57 +0530

    Updated project

--- src/App.tsx
+++ src/App.tsx
@@ -106,7 +106,7 @@ function App() {
                 >
                   <use href="/icons.svg#bluesky-icon"></use>
                 </svg>
-                Bluesky
+                Blacksky
               </a>
             </li>
           </ul>
